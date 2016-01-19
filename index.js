24d664f36541d390311ee8c90981f2cb2e61902c
jackzj <ybybzj@gmail.com>
2016-01-19 15:58:38 +0800

    optimize module path resolving on server side

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ var transformers = require('./lib/transformers');
 var resolver = require('./lib/resolver');
 var depsStreamMiddleware = require('./lib/depsStreamMiddleware');
 var scriptsMiddleware = require('./lib/scriptsMiddleware');
+var _ = require('./lib/util');
 // var pUtil = require('path');
 module.exports = function(app, options) {
   var routePath = options.routePath || '/m',
     loaderPath = options.loaderPath || '/mloader.js',
     pathSettings = options.pathSettings,
-    transformerSettings = options.transformerSettings;
+    transformerSettings = options.transformerSettings,
+    resolverFns = _.makeResolverFns(pathSettings);
   // pathSettings.base = pUtil.resolve(app.get('$boot_dir'), pathSettings.base);
   var makeDepsStream = createDepsStreamer({
     transformers: [
       transformers.addComma,
-      transformers.wrapCMD(pathSettings, transformerSettings && transformerSettings.cmdWrapper),
-      transformers.wrapCSS(pathSettings, transformerSettings && transformerSettings.cssWrapper)
+      transformers.wrapCMD(resolverFns, transformerSettings && transformerSettings.cmdWrapper),
+      transformers.wrapCSS(resolverFns, transformerSettings && transformerSettings.cssWrapper)
     ],
-    depResolver: resolver(pathSettings)
+    depResolver: resolver(resolverFns)
       // resolveDepsCache: resolveCache,
       // contentCache: contentCache
   });
   app.use(loaderPath, scriptsMiddleware(loaderPath, routePath));
-  app.use(routePath, depsStreamMiddleware(makeDepsStream, pathSettings));
+  app.use(routePath, depsStreamMiddleware(makeDepsStream, resolverFns));
 };
 
