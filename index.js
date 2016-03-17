755d956e4260ebbf19e4748ba406858e74b98c5a
jackzj <ybybzj@gmail.com>
2016-03-17 09:05:23 +0800

    support custom transformers; support add globals to client script; update deps-stream;

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 var createDepsStreamer = require('deps-stream');
 
-var transformers = require('./lib/transformers');
+var defaultTransformers = require('./lib/transformers');
 var resolver = require('./lib/resolver');
 var depsStreamMiddleware = require('./lib/depsStreamMiddleware');
 var scriptsMiddleware = require('./lib/scriptsMiddleware');
 var _ = require('./lib/util');
-// var pUtil = require('path');
+var makeTransformer = _.makeTransformer;
+
 module.exports = function(app, options) {
   var routePath = options.routePath || '/m',
     loaderPath = options.loaderPath || '/mloader.js',
     pathSettings = options.pathSettings,
     transformerSettings = options.transformerSettings,
+    extraTransformers = [].concat(options.transformers).filter(Boolean),
     resolverFns = _.makeResolverFns(pathSettings);
   // pathSettings.base = pUtil.resolve(app.get('$boot_dir'), pathSettings.base);
+
+  var transformers = [
+      maketransformer(resolverfns, defaulttransformers.addcomma),
+      maketransformer(resolverfns, defaulttransformers.wrapcmd),
+      maketransformer(resolverfns, defaulttransformers.wrapcss(transformersettings && transformersettings.csswrapper))
+  ];
+
+  if(extraTransformers.length){
+    transformers = transformers.concat(extraTransformers.map(function(exTrans){
+      return makeTransformer(resolverFns, exTrans);
+    }));
+  }
+
   var streamMaker = createDepsStreamer({
-    transformers: [
-      transformers.addComma,
-      transformers.wrapCMD(resolverFns, transformerSettings && transformerSettings.cmdWrapper),
-      transformers.wrapCSS(resolverFns, transformerSettings && transformerSettings.cssWrapper)
-    ],
+    transformers: transformers,
     depResolver: resolver(resolverFns)
       // resolveDepsCache: resolveCache,
       // contentCache: contentCache
   });
-  app.use(loaderPath, scriptsMiddleware(loaderPath, routePath));
+  app.use(loaderPath, scriptsMiddleware(loaderPath, routePath, options.globals));
   app.use(routePath, depsStreamMiddleware(streamMaker, resolverFns));
 };
 
